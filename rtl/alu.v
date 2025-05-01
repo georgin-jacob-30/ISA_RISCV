58bf1672e21a00d2108cac4b7cf785964b0236b7
Georgin Jacob <r1023933@pc-klas1-12.esat.kuleuven.be>
2025-05-01 16:35:29 +0200

    mult support in ALU

--- rtl/alu.v
+++ rtl/alu.v
@@ -32,10 +32,11 @@ module alu #(
    parameter [3:0] SRL_OP = 4'd4;
    parameter [3:0] SUB_OP = 4'd6;
    parameter [3:0] SLT_OP = 4'd7;
+   parameter [3:0] MULT_OP = 4'd8;
 
 
    //REG AND WIRE DECLARATION
-   reg signed [DATA_W-1:0] sub_out,add_out,and_out,or_out,
+   reg signed [DATA_W-1:0] mult_out,sub_out,add_out,and_out,or_out,
                            nor_out,slt_out, sll_out, srl_out;
 	reg 		               overflow_add,overflow_sub,
                            msb_equal_flag;
@@ -59,6 +60,7 @@ module alu #(
 
    //ARITHMETIC and LOGIC OPERATIONS
    always@(*)begin
+      mult_out =  alu_in_0 * alu_in_1;
       add_out  =   alu_in_0 + alu_in_1;
       sll_out  =   alu_in_0 << alu_in_1;
       srl_out  =   alu_in_0 >> alu_in_1;
@@ -80,6 +82,7 @@ module alu #(
 			SLT_OP:  alu_out = slt_out;
 			SLL_OP:  alu_out = sll_out;
 			SRL_OP:  alu_out = srl_out;
+			MULT_OP:  alu_out = mult_out;
 			default: alu_out =     'd0;
 		endcase
 	end
